bc0f2b91ed7c7b1daee7992a5855c91c46a8d49f
Washington Santos <washingtonstk@gmail.com>
2016-08-14 01:41:35 -0300

    Inicia dev

--- shiba.py
+++ shiba.py
@@ -1,1 +1,16 @@
-#!/usr/bin/python
+import os
+
+
+class Info:
+    def __init__(self):
+        self.path = os.path.dirname(os.path.abspath(__file__))
+        self.files = os.listdir(self.path)
+
+    def getfiles(self):
+        sorted(self.files)
+        for i in self.files:
+            print i
+
+if __name__ == "__main__":
+    info = Info()
+    info.getfiles()
