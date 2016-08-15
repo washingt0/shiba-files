c805f5467c4d2adc457c742929ae59bad4726c0a
Washington Santos <washingtonstk@gmail.com>
2016-08-15 20:07:32 -0300

    comeca a criar as estruturas

--- shiba.py
+++ shiba.py
@@ -3,14 +3,31 @@ import os
 
 class Info:
     def __init__(self):
-        self.path = os.path.dirname(os.path.abspath(__file__))
+        self.path = os.path.dirname(os.path.abspath("."))
         self.files = os.listdir(self.path)
 
-    def getfiles(self):
-        sorted(self.files)
+
+    #todo carga de informacoes a partir do os
+    def loadinfo(self):
         for i in self.files:
-            print i
+            objs = []
+            #objs.append(Object(i, )
+
+
+class Object:
+    def __init__(self, nome, owner, group, perm, size, datas):
+        self.info = []
+        self.info.append(nome)
+        self.info.append(owner)
+        self.info.append(group)
+        self.info.append(perm)
+        self.info.append(size)
+        for i in datas:
+            self.info.append(i)
+
+    def getinfo(self):
+        return self.info
+
 
 if __name__ == "__main__":
-    info = Info()
-    info.getfiles()
+    print "Testando"
